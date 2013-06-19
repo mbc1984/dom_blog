8a528d0afb03e67e301c09981ad724edb0384698
Sanya <jobmasalex84@ukr.net>
2013-06-19 15:10:49 +0300

    inicialization commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DomBlog::Application.config.secret_token = '6c36a1817b0ff0bb507eb0f3c7f11a5286cac6c427a1c2235d289a449a9b69036e2b3fba222e4f7f9cd4dae8cde0c1df3287cee567b53e62fa8786548a9a94b4'
